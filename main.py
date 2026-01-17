864b8799602c1a1a8ac7c41159799e6af258210c
ntwaliheritier <ntwalihatsor78.nh@gmail.com>
2026-01-17 02:14:26 +0200

    add brochure generation functionality

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from app.scraper import filter_company_urls
+from app.scraper import generate_brochure
 import json
 
 def main():
-    url_input = input("Enter company website url: ")
-    urls = filter_company_urls(url_input)
-    print(json.loads(urls))
-
+    company_name = input("Enter company name: ")
+    company_url = input("Enter company website url: ")
+    print(generate_brochure(company_name, company_url))
 
 if __name__ == "__main__":
     main()
